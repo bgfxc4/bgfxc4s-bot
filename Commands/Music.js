ff40c84e37a8b3943f9139bfe68c2d5c3dbd535a
bgfxc4 <bindermarc@gmx.de>
2020-03-27 20:20:36 +0100

    test

--- Commands/Music.js
+++ Commands/Music.js
@@ -1,18 +1,22 @@
 const Discord = require('discord.js');
 const RemindTowerFile = './Files.test.mp3'
+const Embeds = require('./embed');
 var dispatcher;
 
 module.exports = {
     cmd_remindTower(msg, args, client){
         
         if (!msg.member.voice.channel) {
+            	Embeds.error('', 'You have to join an Voice Channel first');
+        }else{
+            msg.member.voice.channel.join().then(connection => {
+                
+                dispatcher = connection.play(RemindTowerFile);
+                for(const connection of client.voice.connections.values()){
+                    connection.play(broadcastTower);
+                }
 
-        }
-
-
-        dispatcher = connection.play(RemindTowerFile);
-        for(const connection of client.voice.connections.values()){
-            connection.play(broadcastTower);
+            })
         }
     }
 }
