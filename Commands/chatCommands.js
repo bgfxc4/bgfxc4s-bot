529087e557ab29a6a2a7a08a843ca010f4e4d151
bgfxc4 <bindermarc@gmx.de>
2020-03-31 18:00:09 +0200

    test

--- Commands/chatCommands.js
+++ Commands/chatCommands.js
@@ -3,7 +3,7 @@ const Embeds = require('./embed');
 module.exports = {
 
     cmd_clearChat(msg, args) {
-        Embeds.info(msg.channel, 'Channel cleared!', '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+        Embeds.info(msg.channel, 'Channel cleared!', '`\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`');
     }
 
 }
