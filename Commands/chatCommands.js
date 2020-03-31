8a9d7fc1941522aa7e94241e9bded91a4fc2a873
bgfxc4 <bindermarc@gmx.de>
2020-03-31 17:45:37 +0200

    test

---
+++ Commands/chatCommands.js
@@ -0,0 +1,10 @@
+const Embeds = require('./embed');
+
+module.exports = {
+
+    cmd_clearChat(msg, args) {
+        msg.channel.send(".\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.");
+        Embeds.info(msg.channel, 'Channel cleared!', '');
+    }
+
+}
