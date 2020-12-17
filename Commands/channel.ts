a0b61202c5956f5f7573ef60c4c417d15b94eef4
bgfxc4 <bindermarc@gmx.de>
2020-12-18 00:53:59 +0100

    commmand to move all users now working

--- Commands/channel.ts
+++ Commands/channel.ts
@@ -7,6 +7,27 @@ export function cmd_move_all_to_other_channel(msg: Discord.Message | undefined, 
     if (modus == "get_permission") return perm.list.manage_channels
     if (msg == undefined) return;
 	if (args == undefined || args.length != 2) return embed.error(msg.channel, "You need to specify two arguments [Source Channel, Destination Channel]!", "");
-
+	try {	
+		var arr = msg.guild?.channels.cache.array()
+		var channels:Array<any> = []
+		for (var i = 0; i < 2; i ++) {
+			var found = false;
+			if (!arr) return;
+			for (var j = 0; j < arr.length; j ++) {
+				if (arr[j].name == args[i] && arr[j].type == "voice") {
+					found = true;
+					channels.push(arr[j])
+				}
+			}
+			if (!found) {
+				return embed.error(msg.channel, `There is no voice channel named \`${args[i]}\`!`, "");	
+			}
+		}
+		for (var k in channels[0].members.array()) {
+			channels[0].members.array()[k].edit({ channel: channels[1] })
+		}
+	} catch {
+		return embed.error(msg.channel, "Someting went wrong, please contact the supporter", "")
+	}
 }
 
